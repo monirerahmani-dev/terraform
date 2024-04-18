c20d316725eecfa23f756c9add6ea480a20362da
monirerahmani-dev <monirerahmani-dev@gmail.com>
2024-04-18 13:15:44 +0100

    add variables

--- main.tf
+++ main.tf
@@ -1,11 +1,27 @@
+provider "aws" {}
 
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
+variable cidr_blocks {
+  description = "cidr blocks and name tags for vpc and subnets"
+  type = list(object({
+    cidr_block = string
+    name = string
+  })) 
 }
 
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+variable avail_zone {}
+
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.cidr_blocks[0].cidr_block
+  tags = {
+    Name: var.cidr_blocks[0].name
+  }
+}
+
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  cidr_block = var.cidr_blocks[1].cidr_block
+  availability_zone = var.avail_zone
+  tags = {
+    Name: var.cidr_blocks[1].name
+  }
 }
